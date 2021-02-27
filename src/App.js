df8d74272982a58b30178afc9d4c3a66d4801ea3
Russell <67647336+russellSE16@users.noreply.github.com>
2021-02-27 11:50:45 +0000

    React Query + Routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import AuthApp from "./AuthApp";
+import { useAuthUser } from "./context/auth-context";
+import UnAuthApp from "./UnAuthApp";
+
 export default function App() {
-  return "app";
+  const authUser = useAuthUser();
+
+  if (authUser) {
+    return <AuthApp />
+  } else {
+    return <UnAuthApp />
+  }
 }
